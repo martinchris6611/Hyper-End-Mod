edd021b638ad6edf05184874f5a5c3b055d5de89
Snyperanihilatr <martinchris6611@gmail.com>
2015-06-17 00:08:29 -0500

    Finished Basic PA Multiblock Setup Detection
    Now supports all directions/orientations, also added support for the
    detector block and fixed a few bugs

--- src/main/java/ultimat3/endgamemod/multiblock/BlockStone.java
+++ src/main/java/ultimat3/endgamemod/multiblock/BlockStone.java
@@ -12,6 +12,6 @@ public class BlockStone extends BlockMultiBlock {
 
 	    @Override
 	    public TileEntity createNewTileEntity(World world, int metadata) {
-	        return new TileStoneMultiblock();
+	        return new ParticleAcceleratorMultiblock();
 	    }
 }
